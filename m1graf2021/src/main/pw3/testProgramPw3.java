0925f87d8d6169f0b473f973ddb37b68c71bad58
Antax1 <antoine.claudel2001@gmail.com>
2021-12-07 16:11:38 +0100

    mon ptit commit

--- m1graf2021/src/main/pw3/testProgramPw3.java
+++ m1graf2021/src/main/pw3/testProgramPw3.java
@@ -1,4 +1,18 @@
 package main.pw3;
 
+import main.m1graf2021.UndirectedGraf;
+
+import java.util.Scanner;
+
 public class testProgramPw3 {
+    public static void main(String[] args) throws java.io.FileNotFoundException, java.io.IOException{
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the path to a dot file");
+        String path = input.next();
+        try{
+            ChinesePostman cp = new ChinesePostman(new UndirectedGraf(path));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
